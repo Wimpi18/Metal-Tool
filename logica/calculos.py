66375e623244d3541b2aee1c3892f4228574e263
Jairo Andree Maida Rojas <jairotrabaja123@gmail.com>
2024-12-17 14:26:47 -0400

    feat: ploting metal bar cuts

--- logica/calculos.py
+++ logica/calculos.py
@@ -1,6 +1,16 @@
 from math import sqrt
 from pulp import LpProblem, LpMinimize, LpVariable, lpSum, value
 from itertools import combinations_with_replacement
+import matplotlib.pyplot as plt
+SOLVER_MILO = "highs"
+SOLVER_MINLO = "ipopt"
+
+from amplpy import AMPL, ampl_notebook
+
+ampl = ampl_notebook(
+    modules=["coin", "highs"],  # modules to install
+    license_uuid="default",  # license to use
+) 
 
 def calcular_resultados(datos):
     alto = datos["alto"]
@@ -98,6 +108,31 @@ def simulacion_de_cortes(cantidades, longitudes, longitud_base, costo_por_metro)
     """
     Simula los cortes necesarios para cumplir con las órdenes de perfiles HEB300, minimizando el desperdicio.
     """
+
+    stocks = {
+    "A": {"length": 5, "cost": 6},
+    "B": {"length": 10, "cost": 9},
+    "C": {"length": 15, "cost": 10},
+    }
+
+    finish = {
+        "Viga": {"length": longitudes[0], "demand": cantidades[0]},
+        "Tirante": {"length": longitudes[1], "demand": cantidades[1]},
+        "Pendolón": {"length": longitudes[2], "demand": cantidades[2]},
+        "Montante": {"length": longitudes[3], "demand": cantidades[3]},
+        "Tornapunta": {"length": longitudes[4], "demand": cantidades[4]},
+    }
+
+    patterns = make_patterns(stocks, finish)
+    print(patterns)
+
+    ax = plot_patterns(stocks, finish, patterns)
+
+    x, cost = cut_patterns(stocks, finish, patterns)
+
+    ax = plot_nonzero_patterns(stocks, finish, patterns, x, cost)
+
+
     # Generar todos los patrones viables de cortes
     patrones = generar_patrones(longitudes, longitud_base)
     num_patrones = len(patrones)
@@ -133,3 +168,151 @@ def simulacion_de_cortes(cantidades, longitudes, longitud_base, costo_por_metro)
     )
 
     return barras_usadas, combinaciones_cortes, costo_total, desperdicio_total
+
+
+
+
+def make_patterns(stocks, finish):
+    """
+    Generates patterns of feasible cuts from stock lengths to meet specified finish lengths.
+
+    Parameters:
+    stocks (dict): A dictionary where keys are stock identifiers and values are dictionaries
+                   with key 'length' representing the length of each stock.
+
+    finish (dict): A dictionary where keys are finish identifiers and values are dictionaries
+                   with key 'length' representing the required finish lengths.
+
+    Returns:
+    patterns (list): A list of dictionaries, where each dictionary represents a pattern of cuts.
+                   Each pattern dictionary contains 'stock' (the stock identifier) and 'cuts'
+                   (a dictionary where keys are finish identifiers and the value is the number
+                   of cuts from the stock for each finish).
+    """
+
+    patterns = []
+    for f in finish:
+        feasible = False
+        for s in stocks:
+            # max number of f that fit on s
+            num_cuts = int(stocks[s]["length"] / finish[f]["length"])
+
+            # make pattern and add to list of patterns
+            if num_cuts > 0:
+                feasible = True
+                cuts_dict = {key: 0 for key in finish.keys()}
+                cuts_dict[f] = num_cuts
+                patterns.append({"stock": s, "cuts": cuts_dict})
+
+        if not feasible:
+            print(f"No feasible pattern was found for {f}")
+            return []
+
+    return patterns
+
+
+def plot_patterns(stocks, finish, patterns):
+    # set up figure parameters
+    lw = 0.6
+    cmap = plt.get_cmap("tab10")
+    colors = {f: cmap(k % 10) for k, f in enumerate(finish.keys())}
+    fig, ax = plt.subplots(1, 1, figsize=(8, 0.05 + 0.4 * len(patterns)))
+
+    for k, pattern in enumerate(patterns):
+        # get stock key/name
+        s = pattern["stock"]
+
+        # plot stock as a grey background
+        y_lo = (-k - lw / 2, -k - lw / 2)
+        y_hi = (-k + lw / 2, -k + lw / 2)
+        ax.fill_between((0, stocks[s]["length"]), y_lo, y_hi, color="k", alpha=0.1)
+
+        # overlay finished parts
+        xa = 0
+        for f, n in pattern["cuts"].items():
+            for j in range(n):
+                xb = xa + finish[f]["length"]
+                ax.fill_between((xa, xb), y_lo, y_hi, alpha=1.0, color=colors[f])
+                ax.plot((xb, xb), (y_lo[0], y_hi[0]), "w", lw=1, solid_capstyle="butt")
+                ax.text(
+                    (xa + xb) / 2,
+                    -k,
+                    f,
+                    ha="center",
+                    va="center",
+                    fontsize=6,
+                    color="w",
+                    weight="bold",
+                )
+                xa = xb
+
+    # clean up axes
+    ax.spines[["top", "right", "left", "bottom"]].set_visible(False)
+    ax.set_yticks(
+        range(0, -len(patterns), -1),
+        [pattern["stock"] for pattern in patterns],
+        fontsize=8,
+    )
+
+    return ax
+
+# Given dictionaries of stocks and finished parts, and a list of patterns,
+# find minimum choice of patterns to cut
+
+
+def cut_patterns(stocks, finish, patterns):
+    m = AMPL()
+
+    m.eval(
+        """
+        set S;
+        set F;
+        set P;
+
+        param c{P};
+        param a{F, P};
+        param demand_finish{F};
+
+        var x{P} integer >= 0;
+
+        minimize cost:
+            sum{p in P} c[p]*x[p];
+
+        subject to demand{f in F}:
+            sum{p in P} a[f,p]*x[p] >= demand_finish[f];
+
+    """
+    )
+
+    m.set["S"] = list(stocks.keys())
+    m.set["F"] = list(finish.keys())
+    m.set["P"] = list(range(len(patterns)))
+
+    s = {p: patterns[p]["stock"] for p in range(len(patterns))}
+    c = {p: stocks[s[p]]["cost"] for p in range(len(patterns))}
+    m.param["c"] = c
+    a = {
+        (f, p): patterns[p]["cuts"][f]
+        for p in range(len(patterns))
+        for f in finish.keys()
+    }
+    m.param["a"] = a
+    m.param["demand_finish"] = {
+        f_part: finish[f_part]["demand"] for f_part in finish.keys()
+    }
+
+    m.option["solver"] = SOLVER_MILO
+    m.get_output("solve;")
+
+    return [m.var["x"][p].value() for p in range(len(patterns))], m.obj["cost"].value()
+
+def plot_nonzero_patterns(stocks, finish, patterns, x, cost):
+    k = [j for j, _ in enumerate(x) if _ > 0]
+    ax = plot_patterns(stocks, finish, [patterns[j] for j in k])
+    ticks = [
+        f"{x[k]} x {pattern['stock']}" for k, pattern in enumerate(patterns) if x[k] > 0
+    ]
+    ax.set_yticks(range(0, -len(k), -1), ticks, fontsize=8)
+    ax.set_title(f"Cost = {round(cost,2)}", fontsize=10)
+    return ax
+
